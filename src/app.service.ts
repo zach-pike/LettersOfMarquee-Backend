b062c8d35b52f25d544b500073f68e4a71a303d9
Zachary Pike <zachturnpike@gmail.com>
2024-10-08 16:00:16 -0400

    Cleanup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  getUsernameSearch(dfsd: string) {
-    return {
-      name: "joe",
-      addy: "1337 leet st",
-      pone: "4204206969"
-    }
-  }
-}
+export class AppService {}
